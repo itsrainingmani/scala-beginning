c2c2fb5150ce09eac95ebfa21c0869712747678f
Manikandan Sundararajan <mtiruman@buffalo.edu>
2017-07-02 19:41:58 -0400

    First commit

--- src/main/scala/tutorial/HelloWorld.scala
+++ src/main/scala/tutorial/HelloWorld.scala
@@ -22,5 +22,17 @@ object HelloWorld extends App{
 
   val donutByte: Byte = 0xa
 
+  val favDonut = "Glazed Donut"
+//  println(s"My favorite donut is $favDonut")
 
+//  println("\nStep 2: Using String interpolation on objects")
+//  case class Donut(name: String, tasteLevel: String)
+
+  def max(x: Int, y: Int): Int =
+    if (x>y) x else y
+
+  args.foreach(arg => println(arg))
+//  println(max(3,5))
 }
+
+
