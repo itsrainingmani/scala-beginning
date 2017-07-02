fdbcab111dd02903f53788d1350f23ab990cfa90
Manikandan Sundararajan <mtiruman@buffalo.edu>
2017-07-02 12:55:22 -0400

    First commit

--- src/main/scala/tutorial/HelloWorld.scala
+++ src/main/scala/tutorial/HelloWorld.scala
@@ -4,5 +4,23 @@ package tutorial
   * Created by zeus on 7/1/17.
   */
 object HelloWorld extends App{
-  println("Hello world")
+  var capital = Map("US" -> "Washington", "France" -> "Paris")
+  capital += ("Japan" -> "Tokyo")
+//  println(capital("France"))
+
+  def factorial(x: BigInt): BigInt =
+    if (x==0) 1 else x * factorial(x-1)
+
+  var msg = "Hello, World"
+
+  var donutsToBuy: Int = 5
+  donutsToBuy = 10
+
+  lazy val donutService = "blah"
+  var leastFavorite: String = _
+  leastFavorite = "Blah"
+
+  val donutByte: Byte = 0xa
+
+
 }
---
+++ src/main/scala/tutorial_1/HelloWorldMain.scala
@@ -0,0 +1,10 @@
+package tutorial_1
+
+/**
+  * Created by zeus on 7/2/17.
+  */
+object HelloWorldMain {
+  def main(args: Array[String]): Unit = {
+    println("Hello World from Main")
+  }
+}
